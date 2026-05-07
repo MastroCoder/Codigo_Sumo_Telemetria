f33b706d4c7f66ac3b49f51a1ce0a8c5d78b7dff
MastroCoder <fe03mastromauro@gmail.com>
2026-05-07 20:25:59 -0300

    ✨ feat(lib/Detectors/Detectors.{hpp, cpp}): Implements standalone detectors for enemies and line

---
+++ lib/Detectors/Detectors.cpp
@@ -0,0 +1,54 @@
+#include "Detectors.hpp"
+
+const std::map<unsigned int, unsigned int> sensor_to_bit = {
+  {S1, S1_BIT}, {S2, S2_BIT}, {S3, S3_BIT}, {S4, S4_BIT}, {S5, S5_BIT}
+};
+
+const unsigned int qtr_bits[QTR_COUNT] = {QTR1_BIT, QTR2_BIT};
+
+Detector::Detector(){
+  event_group = xEventGroupCreate();
+}
+
+void EnemyDetector::Detect(){
+  for (const auto &[sensor, bit] : sensor_to_bit){
+    if (digitalRead(sensor)) xEventGroupSetBits(event_group, bit);
+    else xEventGroupClearBits(event_group, bit);
+  }
+};
+
+LineDetector::LineDetector(){
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){QTR1, QTR2}, QTR_COUNT);
+}
+
+// provavelmente deveria ter salvaguardas se chamarem a funcao mais de uma vez
+void LineDetector::Calibrate(QTRCalibrate option){
+  if (nvs.StartStorage(NVS_READWRITE) != ESP_OK) return;
+  if (option == QTRCalibrate::kUseNVS){
+    for (uint8_t i = 0; i < QTR_COUNT; i++){
+      nvs.ReadUInt16(kMinOnKeys[i], &qtr.calibrationOn.minimum[i]);
+      nvs.ReadUInt16(kMaxOnKeys[i], &qtr.calibrationOn.maximum[i]);
+      nvs.ReadUInt16(kMinOffKeys[i], &qtr.calibrationOff.minimum[i]);
+      nvs.ReadUInt16(kMaxOffKeys[i], &qtr.calibrationOff.maximum[i]);
+    }
+  }
+  else if (option == QTRCalibrate::kCalibrate){
+    for(uint16_t i = 0; i <= 800; i++){
+      qtr.calibrate();
+    }
+    for (uint8_t i = 0; i < QTR_COUNT; i++){
+      nvs.WriteUInt16(kMinOnKeys[i], &qtr.calibrationOn.minimum[i]);
+      nvs.WriteUInt16(kMaxOnKeys[i], &qtr.calibrationOn.maximum[i]);
+      nvs.WriteUInt16(kMinOffKeys[i], &qtr.calibrationOff.minimum[i]);
+      nvs.WriteUInt16(kMaxOffKeys[i], &qtr.calibrationOff.maximum[i]);
+    }
+  }
+}
+
+void LineDetector::Detect(){
+  for (uint8_t i = 0; i < QTR_COUNT; i++){
+    if (qtr_values[i] >= 750) xEventGroupSetBits(event_group, qtr_bits[i]);
+    else xEventGroupClearBits(event_group, qtr_bits[i]);
+  }  
+}
---
+++ lib/Detectors/Detectors.hpp
@@ -0,0 +1,44 @@
+#ifndef LIB_DETECTORS_HPP_
+#define LIB_DETECTORS_HPP_
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+#include "types.hpp"
+#include <QTRSensors.h>
+#include <map>
+#include "pins.h"
+#include "internal_defs.h"
+#include <Arduino.h>
+#include "NVSHandler.hpp"
+
+class Detector {
+  public:
+    EventGroupHandle_t event_group;
+    Detector();
+    ~Detector() = default;
+    virtual void Detect() = 0;
+};
+
+class EnemyDetector : Detector {
+  public:
+    void Detect() override; 
+};
+
+class LineDetector : Detector {
+  private:
+    QTRSensors qtr;
+    uint16_t qtr_values[QTR_COUNT];
+    // Isso deveria ser visível ao usuário?
+    const char* kMinOnKeys[QTR_COUNT] = {"min_on_1", "min_on_2"};
+    const char* kMaxOnKeys[QTR_COUNT] = {"max_on_1", "max_on_2"};
+    const char* kMinOffKeys[QTR_COUNT] = {"min_off_1", "min_off_2"};
+    const char* kMaxOffKeys[QTR_COUNT] = {"max_off_1", "max_off_2"};
+    NVSHandler nvs = NVSHandler("QTR"); // meio feio, não? Melhor definir um default e um pra usar bonitinho?
+
+  public:
+    LineDetector();
+    void Calibrate(QTRCalibrate option);
+    void Detect() override;
+};
+
+#endif
